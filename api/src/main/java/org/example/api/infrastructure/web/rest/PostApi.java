1d4b467d2bb26d9cdc320eb05d53fe2277d9a4df
yongckim <zxcv9203@naver.com>
2024-03-26 20:51:08 +0900

    :art: 불필요한 테스트 삭제 및 코드 포맷팅 진행

--- api/src/main/java/org/example/api/infrastructure/web/rest/PostApi.java
+++ api/src/main/java/org/example/api/infrastructure/web/rest/PostApi.java
@@ -2,14 +2,10 @@ package org.example.api.infrastructure.web.rest;
 
 import jakarta.validation.Valid;
 import org.example.api.infrastructure.web.rest.request.PostCreate;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class PostApi {
