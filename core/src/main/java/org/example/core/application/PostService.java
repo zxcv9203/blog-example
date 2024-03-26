05b213a534bc73328a574722d9e0fc39ceba0af0
yongckim <zxcv9203@naver.com>
2024-03-26 22:00:14 +0900

    :tada: common 모듈 추가

---
+++ core/src/main/java/org/example/core/application/PostService.java
@@ -0,0 +1,13 @@
+package org.example.core.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PostService {
+
+    public void write() {
+
+    }
+}
