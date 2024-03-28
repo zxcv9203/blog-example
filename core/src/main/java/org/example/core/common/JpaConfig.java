4710c13efd31e8cb5c5dcd17584c9cda614355a3
yongckim <zxcv9203@naver.com>
2024-03-28 18:29:46 +0900

    :bug: 멀티 모듈에서 JPA 사용을 하기 위해 패키지 스캔 추가

---
+++ core/src/main/java/org/example/core/common/JpaConfig.java
@@ -0,0 +1,11 @@
+package org.example.core.common;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "org.example.core.domain")
+@EntityScan(basePackages = "org.example.core.domain")
+public class JpaConfig {
+}
