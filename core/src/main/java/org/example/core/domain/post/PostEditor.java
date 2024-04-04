4ac3b071999ea8cf91839f29470ad584c412c420
yongckim <zxcv9203@naver.com>
2024-04-04 21:23:23 +0900

    :recycle: 값이 들어오지 않은 경우에 않았다면 null로 값을 변경하지 않도록 PostEditor 수정

--- core/src/main/java/org/example/core/domain/post/PostEditor.java
+++ core/src/main/java/org/example/core/domain/post/PostEditor.java
@@ -2,15 +2,42 @@ package org.example.core.domain.post;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
 public class PostEditor {
 
     private final String title;
     private final String content;
 
+    public static PostEditorBuilder builder() {
+        return new PostEditorBuilder();
+    }
+
+    public static class PostEditorBuilder {
+        private String title;
+        private String content;
+
+        PostEditorBuilder() {
+        }
+
+        public PostEditorBuilder title(final String title) {
+            if (title != null) {
+                this.title = title;
+            }
+            return this;
+        }
+
+        public PostEditorBuilder content(final String content) {
+            if (content != null) {
+                this.content = content;
+            }
+            return this;
+        }
+
+        public PostEditor build() {
+            return new PostEditor(this.title, this.content);
+        }
+    }
 }
