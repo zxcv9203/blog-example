d84518c3fb7b13c94fbc14b4b30ce51b2daa082e
yongckim <zxcv9203@naver.com>
2024-04-13 19:45:14 +0900

    :heavy_plus_sign: bootstrap util 의존성 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import "bootstrap/dist/css/bootstrap-utilities.css"
 
 createApp(App)
     .use(router)
