4a8caabd868199873d16c80e126e3b39a1a934a2
yongckim <zxcv9203@naver.com>
2024-04-13 20:15:42 +0900

    :sparkles: 글 작성 페이지 구현

--- frontend/src/views/WriteView.vue
+++ frontend/src/views/WriteView.vue
@@ -1,13 +1,32 @@
 <script setup lang="js">
+
 import {ref} from "vue";
 
-const count = ref(0);
+import axios from 'axios';
+
+const title = ref("")
+const content = ref("")
+
+const write = function () {
+  axios.post("http://localhost:8080/posts", {
+    "title": title.value,
+    "content": content.value
+  })
+}
 </script>
 
 <template>
-  <p>안녕하세요</p>
-  <button @click="count++">Click me</button>
-  <p>{{ count }}</p>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력하세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows=15 placeholder="내용을 입력하세요"></el-input>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">저장</el-button>
+  </div>
 </template>
 
 <style scoped>
