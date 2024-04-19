fab6383f6458ce08d66f363c2d08807c7c58077c
yongckim <zxcv9203@naver.com>
2024-04-19 22:25:00 +0900

    :sparkles: excludePathPatterns를 추가하여 검증하지 않는 API 엔드 포인트를 지정

--- api/src/main/java/org/example/api/common/config/WebMvcConfig.java
+++ api/src/main/java/org/example/api/common/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor());
+        registry.addInterceptor(new AuthInterceptor())
+                .excludePathPatterns("/test");
     }
 }
