7b0d051d21f8ef06f9d7bb28f8c643167a97836d
yongckim <zxcv9203@naver.com>
2024-04-27 03:51:39 +0900

    :white_check_mark: 테스트를 위한 세션 데이터 추가

--- api/src/test/resources/db/setup.sql
+++ api/src/test/resources/db/setup.sql
@@ -1,1 +1,3 @@
-INSERT INTO `member` (name, email, password, created_at) VALUES ('admin', 'admin@example.com', '1234', now())
+INSERT INTO `member` (name, email, password, created_at) VALUES ('admin', 'admin@example.com', '1234', now());
+
+INSERT INTO `session` (access_token, member_id) VALUES ('6392da07-d89c-4c26-936a-53590471bdbe', 1);
