898c11b81b70217be7e2672655f870fa9872144b
yongckim <zxcv9203@naver.com>
2024-04-27 03:54:35 +0900

    :sparkles: 데이터베이스에서 세션 인증을 진행하도록 구현

--- api/src/main/java/org/example/api/common/config/AuthResolver.java
+++ api/src/main/java/org/example/api/common/config/AuthResolver.java
@@ -1,14 +1,21 @@
 package org.example.api.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.api.common.config.model.UserSession;
 import org.example.core.common.exception.UnauthorizedException;
+import org.example.core.domain.auth.Session;
+import org.example.core.domain.auth.SessionRepository;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final SessionRepository sessionRepository;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return UserSession.class.equals(parameter.getParameterType());
@@ -17,12 +24,12 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         String accessToken = webRequest.getHeader("Authorization");
-        if (!"".equals(accessToken)) {
+        if ("".equals(accessToken)) {
             throw new UnauthorizedException("인증되지 않은 사용자입니다.");
         }
 
-        // TODO 데이터베이스 사용자 확인 작업
+        Session session = sessionRepository.findByAccessToken(accessToken);
 
-        return new UserSession(1L);
+        return new UserSession(session.getId());
     }
 }
